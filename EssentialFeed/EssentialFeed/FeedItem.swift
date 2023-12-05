18fed066d6636ebfdef72adbbc7f1dd167952851
Kern Jackson <kernjackson@gmail.com>
2023-12-05 13:32:29 -0800

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Kern Jackson on 12/5/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
