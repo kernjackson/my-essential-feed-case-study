b1d78502c54350b7f559a98f8915a581d79831ac
Kern Jackson <kernjackson@gmail.com>
2023-12-07 14:49:03 -0800

    Move HTTPClient to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClientResult.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClientResult.swift
+//  EssentialFeed
+//
+//  Created by Kern Jackson on 12/7/23.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
