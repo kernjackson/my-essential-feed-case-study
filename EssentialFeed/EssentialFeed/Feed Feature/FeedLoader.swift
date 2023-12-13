9ff55e8bc5966b3422a964de78954c47b00a7d71
Kern Jackson <kernjackson@gmail.com>
2023-12-13 10:22:24 -0800

    Change indentation from spaces to tabs to facilitate diffing against essential-feed-case-study

--- EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public enum LoadFeedResult {
-    case success([FeedItem])
-    case failure(Error)
+	case success([FeedItem])
+	case failure(Error)
 }
 
 public protocol FeedLoader {
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+	func load(completion: @escaping (LoadFeedResult) -> Void)
 }
