2c32e1202ca9f72db74d602e2e5a433dfdb7f662
Kern Jackson <kernjackson@gmail.com>
2023-12-21 16:49:24 -0800

    Add line spaces to switch for better legibility

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -38,11 +38,14 @@ public final class LocalFeedLoader {
 			case let .failure(error):
 				self.store.deleteCachedFeed { _ in }
 				completion(.failure(error))
+				
 			case let .found(feed, timestamp) where validate(timestamp):
 				completion(.success(feed.toModels()))
+				
 			case .found:
 				self.store.deleteCachedFeed { _ in }
 				completion(.success([]))
+				
 			case .empty:
 				completion(.success([]))
 			}
